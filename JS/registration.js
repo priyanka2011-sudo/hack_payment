28895a1366bc6d2a1879b99b2a5edc6c1b249279
Sina Monajemi <sina.monajemi@me.com>
2020-06-27 20:30:15 -0400

    third commit

--- JS/registration.js
+++ JS/registration.js
@@ -18,11 +18,24 @@ function emailvalidation()
 
 }
 
+function phonevalidation()
+     {
+        var input = document.form.phonenum.value;
+        while(input >= 10)
+        {
+            if(input.length >= 10)
+            break;
+            else
+            alert("Phone Number cannot be less than 10 digits"); return false;
+        }      
+        return true;            
+    }
  
 function isValid()
 {
     var flag = true;
     flag &= emailvalidation();
+    flag &= phonevalidation();
     if (flag)   
         return true;    
     else   
