500836af643bcc7cd55b9556e2ecda8749bf79c7
Sina Monajemi <sina.monajemi@me.com>
2020-06-30 00:53:17 -0400

    Update CustomerList2.php

--- CustomerList2.php
+++ CustomerList2.php
@@ -1,5 +1,5 @@
 <?php include "header.php"; ?>
-        <a href="CustomerRegistration.php"><button id="create_customer" class="functional_button">Create Customer</button></a>
+        <a href="CustomerRegistration.php"><button id="create_customer" class="functional_button">Create Customer</button></a><br>
         <button id="scan_barcode" class="functional_button">Scan Barcode</button>
         <input type="text" id="search_by_phone" class="search_by" placeholder="Search by phone number">
         <section id="profile_list">
