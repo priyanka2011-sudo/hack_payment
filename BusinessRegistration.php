a9a03f3d9e68ee6f342d88af10e51adf75249306
Sina Monajemi <sina.monajemi@me.com>
2020-06-30 01:29:42 -0400

    commit

--- BusinessRegistration.php
+++ BusinessRegistration.php
@@ -235,7 +235,7 @@ if(isset($_POST['BusinessName'])){
 
                 <div class='col-25'>
                 <label for="BusinessLogo">Logo</label></div>
-                <img id="logo" src="Media\business_logo/logo.png"  alt="LOGO">
+                <img id="logo" class ="resize" src="Media\business_logo/logo.png"  alt="LOGO">
                 <div class='col-50'>
                 <input type="file" placeholder="BusinessLogo" name="BusinessLogo" id="BusinessLogo" required>
                 </div> 
