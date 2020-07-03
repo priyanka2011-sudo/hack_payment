bb1fafdf5d0e404764173592299fe843b0bcd010
Sina Monajemi <sina.monajemi@me.com>
2020-07-03 06:31:30 -0400

    home commit

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     
     <script src="js/prregistration.js"></script>
+    <script src="js/typed.js"></script>
   </head>
 <body>
 
